b08e5c7f2495f7ff8767a901ccc05272a2be3f59
Molly McCarron <mccarronmollye@gmail.com>
2019-01-10 23:24:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,5 +43,12 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end 
   end 
+  
+  def update
+    sql = <<-SQL
+    
+    
+    SQL
+  end 
 
 end
